95681c382e16aa836b02b75c528d2cb9cb723de2
Bivek Gurung <bivek.tamu@gmail.com>
2023-09-28 10:08:23 +1000

    Ticker tape feature almost done

--- src/component/Ticker/Ticker.js
+++ src/component/Ticker/Ticker.js
@@ -1,45 +1,77 @@
-import React, { useEffect, useState } from 'react'
+import React, { useEffect, useState, useRef } from 'react'
 import './Ticker.scss'
 const Ticker = ({text}) => {
 
+  const pRef = useRef()
 
-  useEffect(()=> {
+  const [pEle, setPele] = useState(pRef.current)
+
+  
+  let p = pRef.current
+  // wrapper.style.width = `${max}px`
+
+  useEffect(()=>{
+    if(pEle) {
     initateTicerkTape()
-  }, [text])
+    }
+    else {
+      setPele(pRef.current)
+    }
+  }, [pEle])
 
 
   function initateTicerkTape() {
-
-    const words = text.split(' ')
-    console.log(words)
     
     const wrapper = document.getElementById('wrapper')
     let w = 0, max = window.innerWidth*2
+
+
+    const p = pEle
+    console.log(p.clientWidth)
     // wrapper.style.width = `${max}px`
 
-    do {
-      const p = document.querySelector('#wrapper > p')
+    
+    // do {
+      for(let i = 0; i <3; i++) {
       const newP = document.createElement('p')
-      newP.innerHTML = p.innerHTML + ' '
+      newP.innerHTML = p.innerHTML
       // newP.style.width = `${p.clientWidth}px`
       wrapper.appendChild(newP)
-      console.log(p.clientWidth)
 //       
 //       // console.log(wrapper.clientWidth)
-       w = w + p.clientWidth
+       w = p.clientWidth * (i+1)
+  }
+w= w + p.clientWidth
+wrapper.style.width = `${w}px`
+console.log(w)
+
+wrapper.classList.add('animate')
+
+wrapper.style.animation = `
+@keyframes move {
+  0% {
+      transform: translateX(0);
+  }
+
+  100% {
+      transform: translateX(calc(-50%  + 524px));
+  }
+}
+`
+
+
 // 
 //        console.log(w)
-    } while (w < max);
-
-  console.log('max', max)
+    // } while (w < max + p.clientWidth);
 
 
   }
 
+  
   return (
     <div id="ticker_tape">
       <div id="wrapper">
-        <p>{text}</p>
+        <p ref={pRef}>{text}</p>
       </div>
     </div>
   )
