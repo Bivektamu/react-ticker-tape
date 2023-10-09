1fc279ef8bf06931f61fd1f7cda7a99722a7c0fc
Bivek <bivek.tamu@gmail.com>
2023-10-09 16:18:56 +1100

    Ticker tape feature done. Now, prepping for react library

--- src/App.js
+++ src/App.js
@@ -3,9 +3,7 @@ import React from "react";
 import { Ticker } from "./component";
 function App() {
   return (
-    // <Ticker text='Hi there! This is ticker tape version 1. Please check documentation on how to embed this library to your react project. Cheers!' timer='20' />
-    <Ticker text='Hi there! This is ticker tape version 1. ' timer='10' bg='#000' color='#f3f3f3' />
-
+    <Ticker text='Hi there! This is ticker tape version 1. Please check documentation on how to embed this library to your react project. Cheers!' timer='10' bg='red' color='white' />
   );
 }
 
--- src/component/Ticker/Ticker.js
+++ src/component/Ticker/Ticker.js
@@ -1,31 +1,22 @@
 import React, { useEffect, useState, useRef } from "react";
 import "./Ticker.scss";
-const Ticker = ({ text, timer, bg, color }) => {
+const Ticker = ({ text, timer, bg, color, fix }) => {
   const pRef = useRef();
 
   const [pEle, setPele] = useState(pRef.current);
   const [wD, setWd] = useState(null);
-  const time = timer || 10
-  const bgC = bg || 'blueviolet'
-  const colorC = color || 'white'
-
-
-
-
-
+  const time = timer || 10;
+  const bgC = bg || "blueviolet";
+  const colorC = color || "white";
   useEffect(() => {
-
-    const pTags = document.querySelectorAll('#wrapper > p')
+    const pTags = document.querySelectorAll("#wrapper > p");
     if (pEle && wD && pTags.length == 1) {
       initateTicerkTape();
-    }
-    else {
+    } else {
       setPele(pRef.current);
     }
-
   }, [pEle, wD]);
 
-  
   function initateTicerkTape() {
     const wrapper = document.getElementById("wrapper");
     let w = 0,
@@ -39,7 +30,7 @@ const Ticker = ({ text, timer, bg, color }) => {
       newP.innerHTML = p.innerHTML;
       wrapper.appendChild(newP);
       i++;
-      console.log(wD)
+      console.log(wD);
       w = wD * (i + 0);
     } while (w < max);
 
@@ -52,17 +43,44 @@ const Ticker = ({ text, timer, bg, color }) => {
     w = w + wD;
 
     wrapper.style.width = `${w}px`;
-    wrapper.style.height = `${p.clientHeight}px`
-    console.log(wrapper.clientHeight)
-    console.log(p.clientHeight)
-    wrapper.style.animation = `move ${time}s linear infinite`
+    wrapper.style.height = `${p.clientHeight + 10}px`;
+    wrapper.style.animation = `move ${time}s linear infinite`;
+    wrapper.style.background = bgC;
+    wrapper.style.color = colorC;
+    console.log();
+    if (fix) {
+      let parent = wrapper.parentElement;
+      parent.style.position = "fixed";
+      switch (fix) {
+        case "top":
+          parent.style.top = "0px";
+          break;
+        case "bottom":
+          parent.style.bottom = "0px";
+          break;
+
+        case "left":
+          parent.style.left = "0px";
+          parent.style.bottom = `-${wrapper.clientHeight}px`;
+          parent.style.transform = "rotate(-90deg)";
+          parent.style.transformOrigin = "left top";
+          break;
+
+        case "right":
+          parent.style.bottom = `-${wrapper.clientHeight}px`;
+          parent.style.left = `calc(100vw - ${wrapper.clientHeight}px)`;
+          parent.style.transform = "rotate(-90deg)";
+          parent.style.transformOrigin = "left top";
+          break;
+        default:
+          break;
+      }
+    }
   }
 
-  window.onload = function() {
-    setTimeout(() => {
-    setWd(pEle.clientWidth)
-  }, 1);
-  }
+  window.onload = function () {
+      setWd(pEle.clientWidth);
+  };
 
   return (
     <div id="ticker_tape">
